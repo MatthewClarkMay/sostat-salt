b005ef4eeed55853fce40b98d57b046705288fb0
Matthew May <mcmay.web@gmail.com>
2018-07-05 15:21:55 -0400

    Create sostat-salt.sh

---
+++ sostat-salt.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo salt '*' cmd.run 'sostat' | tee sostat-salt.txt &&
+
+csplit -f sostat-split- sostat-salt.txt '/Service Status/-2' {*}  
+
+for file in $(ls sostat-split-*); do 
+    WC=$(wc -l $file | awk '{ print $1 }')
+    if [[ $WC -lt 1 ]]; then
+        rm $file
+    fi
+done
+
+for file in $(ls sostat-split-*); do
+    HOST=$(head -1 $file | tr -d ':')
+    mv $file $HOST.txt
+done
